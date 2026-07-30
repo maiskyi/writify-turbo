7c6d2f248638b1bb63e2ad2819eef61e3ea0c4d0
Andrii Maiskyi <andrii.maiskyi@gmail.com>
2026-07-30 17:21:13 +0100

    feat: init. #1

--- apps/keystone/auth.ts
+++ apps/keystone/auth.ts
@@ -40,7 +40,7 @@ const sessionMaxAge = 60 * 60 * 24 * 30
 // you can find out more at https://keystonejs.com/docs/apis/session#session-api
 const session = statelessSessions({
   maxAge: sessionMaxAge,
-  secret: process.env.SESSION_SECRET,
+  secret: process.env.KEYSTONE_SESSION_SECRET!,
 })
 
 export { withAuth, session }
