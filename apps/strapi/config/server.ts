70df44d70b39cd101d1a0a7d8827030d75f846ea
Andrii Maiskyi <andrii.maiskyi@gmail.com>
2026-07-30 17:51:57 +0100

    feat: init. #1

--- apps/strapi/config/server.ts
+++ apps/strapi/config/server.ts
@@ -3,6 +3,7 @@ import type { Core } from '@strapi/strapi';
 const config = ({ env }: Core.Config.Shared.ConfigParams): Core.Config.Server => ({
   host: env('STRAPI_HOST', '0.0.0.0'),
   port: env.int('STRAPI_PORT', 1337),
+  url: env('STRAPI_URL', 'http://cms.writify.localhost'),
   app: {
     keys: env.array('STRAPI_APP_KEYS')!,
   },
